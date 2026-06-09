cb133a4f3fc16dc913fe1c54974fb89a21a3c08a
Natalia Bortoli Vieira <nataliabortolivieira@gmail.com>
2026-06-09 16:49:15 -0300

    Adiciona suporte para controle de Pacientes na aplicação web

--- ControleDeMedicamentosWeb.WebApp/Compartilhado/Infra/Arquivos/ContextoJson.cs
+++ ControleDeMedicamentosWeb.WebApp/Compartilhado/Infra/Arquivos/ContextoJson.cs
@@ -1,65 +1,66 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using ControleDeMedicamentosWeb.WebApp.Modulos.ModuloPaciente.Dominio;
 
 namespace ControleDeMedicamentosWeb.WebApp.Compartilhado.Infra.Arquivos;
 
 public sealed class ContextoJson
 {
-  // public List<Fornecedor> Fornecedores { get; set; } = new List<Fornecedor>();
-  // public List<Paciente> Pacientes { get; set; } = new List<Categoria>();
-  // public List<Medicamento> Medicamentos { get; set; } = new List<Medicamento>();
-  // public List<Funcionario> Funcionarios { get; set; } = new List<Funcionario>();
-  // public List<Estoque> Estoque { get; set; } = new List<Estoque>();
+    // public List<Fornecedor> Fornecedores { get; set; } = new List<Fornecedor>();
+    public List<Paciente> Pacientes { get; set; } = new List<Paciente>();
+    // public List<Medicamento> Medicamentos { get; set; } = new List<Medicamento>();
+    // public List<Funcionario> Funcionarios { get; set; } = new List<Funcionario>();
+    // public List<Estoque> Estoque { get; set; } = new List<Estoque>();
 
-  private readonly string caminhoArquivo;
+    private readonly string caminhoArquivo;
 
-  public ContextoJson()
-  {
-    string caminhoAppData = Environment
-        .GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+    public ContextoJson()
+    {
+        string caminhoAppData = Environment
+            .GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
 
-    string caminhoDiretorio = Path.Combine(caminhoAppData, "ControleDeMedicamentosWeb");
+        string caminhoDiretorio = Path.Combine(caminhoAppData, "ControleDeMedicamentosWeb");
 
-    Directory.CreateDirectory(caminhoDiretorio);
+        Directory.CreateDirectory(caminhoDiretorio);
 
-    caminhoArquivo = Path.Combine(caminhoDiretorio, "dados.json");
-  }
+        caminhoArquivo = Path.Combine(caminhoDiretorio, "dados.json");
+    }
 
-  public void Salvar()
-  {
-    JsonSerializerOptions opcoesJson = new JsonSerializerOptions();
-    opcoesJson.WriteIndented = true;
-    opcoesJson.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-    opcoesJson.ReferenceHandler = ReferenceHandler.Preserve;
-    opcoesJson.Converters.Add(new JsonStringEnumConverter());
+    public void Salvar()
+    {
+        JsonSerializerOptions opcoesJson = new JsonSerializerOptions();
+        opcoesJson.WriteIndented = true;
+        opcoesJson.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        opcoesJson.ReferenceHandler = ReferenceHandler.Preserve;
+        opcoesJson.Converters.Add(new JsonStringEnumConverter());
 
-    string jsonString = JsonSerializer.Serialize(this, opcoesJson);
+        string jsonString = JsonSerializer.Serialize(this, opcoesJson);
 
-    File.WriteAllText(caminhoArquivo, jsonString);
-  }
+        File.WriteAllText(caminhoArquivo, jsonString);
+    }
 
-  public void Carregar()
-  {
-    if (!File.Exists(caminhoArquivo))
-      return;
+    public void Carregar()
+    {
+        if (!File.Exists(caminhoArquivo))
+            return;
 
-    string jsonString = File.ReadAllText(caminhoArquivo);
+        string jsonString = File.ReadAllText(caminhoArquivo);
 
-    JsonSerializerOptions opcoesJson = new JsonSerializerOptions();
-    opcoesJson.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-    opcoesJson.ReferenceHandler = ReferenceHandler.Preserve;
-    opcoesJson.Converters.Add(new JsonStringEnumConverter());
+        JsonSerializerOptions opcoesJson = new JsonSerializerOptions();
+        opcoesJson.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        opcoesJson.ReferenceHandler = ReferenceHandler.Preserve;
+        opcoesJson.Converters.Add(new JsonStringEnumConverter());
 
-    ContextoJson? contextoSalvo = JsonSerializer
-        .Deserialize<ContextoJson>(jsonString, opcoesJson);
+        ContextoJson? contextoSalvo = JsonSerializer
+            .Deserialize<ContextoJson>(jsonString, opcoesJson);
 
-    if (contextoSalvo == null)
-      return;
+        if (contextoSalvo == null)
+            return;
 
-    // Pacientes = contextoSalvo.Pacientes;
-    // Fornecedores = contextoSalvo.Fornecedores;
-    // Medicamentos = contextoSalvo.Medicamentos;
-    // Funcionarios = contextoSalvo.Funcionarios;
-    // Estoque = contextoSalvo.Estoque;
-  }
+        Pacientes = contextoSalvo.Pacientes;
+        // Fornecedores = contextoSalvo.Fornecedores;
+        // Medicamentos = contextoSalvo.Medicamentos;
+        // Funcionarios = contextoSalvo.Funcionarios;
+        // Estoque = contextoSalvo.Estoque;
+    }
 }
